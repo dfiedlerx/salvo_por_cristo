806abfb8270bd7d96f9d3166b5bbd1ee0ded401b
Daniel Fiedler <danielfiedler_@hotmail.com>
2020-01-30 04:58:29 -0300

    Vue to Work

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center" v-on:click="sum()">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component. {{ quantidade }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                quantidade : 0
+            }
+        },
+        methods: {
+            sum: function () {
+                this.quantidade += 1;
+            }
+        }
+
+    }
+</script>
